cbe44b50d136af87a7639376b4df58ee00b5d557
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2022-11-10 18:32:08 -0600

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,21 @@
+const { ethers } = require("hardhat");
+// const hre = require("hardhat");
+
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token");
+
+  // Deploy contract (deploy function from ethers)
+  const token = await Token.deploy();
+  // Get information that was deployed
+  await token.deployed();
+  console.log(`Token Deployed to: ${token.address}`);
+  // Log transaction
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
